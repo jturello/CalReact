0d86ebdaa880df25734795a481e08a11aed70088
James Turello <jturello@yahoo.com>
2017-09-16 15:05:04 -0700

    nest an appointment react component in appointments component

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,15 +1,14 @@
 class Appointments extends React.Component {
   render() {
-    return <div>
-      {this.props.appointments.map(function(appointment) {
-        return (
-          <div>
-            <h3>{appointment.title}</h3>
-            <p>{appointment.appt_time}</p>
-          </div>
-        )
-      })}
-    </div>
+    return (
+      <div>
+        {this.props.appointments.map(function(appointment) {
+          return (
+            <Appointment appointment={appointment} />
+          )
+        })}
+      </div>
+    )
   }
 }
 
